034d4632ee0ec7d383c5851b44d0e2a52420eb82
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-05-03 16:13:45 -0300

    Adiciona enums para status de assinatura, papel de usuário e notificações na configuração do esquema compartilhado

--- src/shared/schema.config.ts
+++ src/shared/schema.config.ts
@@ -1,1 +1,19 @@
+import { pgEnum } from "drizzle-orm/pg-core";
+
+/**
+ * SHARED SCHEMA CONFIGURATION
+ */
+
 export const ID_LENGTH = 36;
+
+/**
+ * USERS ENUMS SCHEMA
+ */
+export const userSignatureStatusEnum = pgEnum("signature_status", [
+  "active",
+  "inactive",
+]);
+
+export const userRoleEnum = pgEnum("role", ["admin", "user"]);
+
+export const notificationsStatusEnum = pgEnum("notifications", ["yes", "no"]);
