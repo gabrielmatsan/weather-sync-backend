a3ce926406a0ab0976b3f3e9965144d930442979
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-05-04 13:03:07 -0300

    feat: adiciona casos de uso para login e registro de usuários

---
+++ src/auth/application/login.usecase.ts
@@ -0,0 +1,37 @@
+import { WrongCredentialsError } from "@/shared/errors/wrong-crendentials-error";
+import { comparePassword } from "@/shared/infra/password";
+import type { IUsersRepository } from "@/users/domain/users-repository.interface";
+import type { User } from "@/users/domain/users.type";
+import { t, type Static } from "elysia";
+
+export const loginUserRequestSchema = t.Object({
+  email: t.String(),
+  password: t.String(),
+});
+
+export type LoginUserRequest = Static<typeof loginUserRequestSchema>;
+
+export type LoginUserResponse = Omit<User, "password">;
+
+export async function loginUseCase(
+  userRepository: IUsersRepository,
+  { email, password }: LoginUserRequest
+): Promise<LoginUserResponse> {
+  const user = await userRepository.findByEmail(email);
+
+  if (!user) {
+    throw new WrongCredentialsError();
+  }
+
+  console.log("passei");
+  const isPasswordValid = await comparePassword(password, user.password);
+
+  if (!isPasswordValid) {
+    throw new WrongCredentialsError();
+  }
+  console.log("passei 2");
+
+  const { password: _, ...userWithoutPassword } = user;
+
+  return userWithoutPassword;
+}
---
+++ src/auth/application/register-user.usecase.ts
@@ -0,0 +1,42 @@
+import { EmailAlreadyUsedError } from "@/shared/errors/email-already-used-error";
+import { WrongCredentialsError } from "@/shared/errors/wrong-crendentials-error";
+import { hashPassword } from "@/shared/infra/password";
+import type { IUsersRepository } from "@/users/domain/users-repository.interface";
+import { UserType } from "@/users/domain/users.type";
+import { t, type Static } from "elysia";
+
+export const registerUserRequestSchema = t.Intersect([
+  t.Omit(UserType, ["id", "createdAt", "updatedAt", "signatureStatus"]),
+  t.Object({
+    // Caso um dia fizer integração com o google, facebook, etc, colocar oauth
+    provider: t.UnionEnum(["credentials"]),
+  }),
+]);
+
+export type RegisterUserRequest = Static<typeof registerUserRequestSchema>;
+
+export async function registerUserUseCase(
+  userRepository: IUsersRepository,
+  userData: RegisterUserRequest
+) {
+  const isUserAlreadyExists = await userRepository.findByEmail(userData.email);
+
+  if (isUserAlreadyExists) {
+    throw new EmailAlreadyUsedError();
+  }
+
+  if (userData.provider === "credentials") {
+    if (!userData.password) {
+      throw new WrongCredentialsError();
+    }
+
+    userData.password = await hashPassword(userData.password.trim());
+  }
+
+  const user = await userRepository.createUser({
+    ...userData,
+    notifications: userData.notifications ?? "yes",
+  });
+
+  return { user };
+}
