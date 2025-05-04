3fb5adf62b385fea2eee5d6c7242a70bcde7cf61
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-05-04 07:03:20 -0300

    feat: Adiciona índices para otimização nas tabelas de poluentes e clima

--- src/pollutants/domain/pollutants.schema.ts
+++ src/pollutants/domain/pollutants.schema.ts
@@ -2,6 +2,7 @@ import { dataSourceSchema } from "@/data-source/domain/data-source.schema";
 import { placesSchema } from "@/places/domain/places.schema";
 import { relations } from "drizzle-orm";
 import {
+  index,
   integer,
   numeric,
   pgTable,
@@ -10,22 +11,27 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 
-export const pollutantsSchema = pgTable("pollutants", {
-  id: serial("id").primaryKey().notNull(),
-  placeId: integer("place_id")
-    .references(() => placesSchema.id)
-    .notNull(),
-  sourceId: integer("source_id")
-    .references(() => dataSourceSchema.id)
-    .notNull(),
-  dominantPollutant: varchar("dominant_pollutant", { length: 255 }).notNull(),
-  coValue: numeric("co_concentration", { precision: 10, scale: 6 }).notNull(),
-  coMetricUnit: varchar("co_metric_unit", { length: 30 }).notNull(),
-  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
-  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-    () => new Date()
-  ),
-});
+export const pollutantsSchema = pgTable(
+  "pollutants",
+  {
+    id: serial("id").primaryKey().notNull(),
+    placeId: integer("place_id")
+      .references(() => placesSchema.id)
+      .notNull(),
+    sourceId: integer("source_id")
+      .references(() => dataSourceSchema.id)
+      .notNull(),
+    dominantPollutant: varchar("dominant_pollutant", { length: 255 }).notNull(),
+    coValue: numeric("co_concentration", { precision: 10, scale: 6 }).notNull(),
+    coMetricUnit: varchar("co_metric_unit", { length: 30 }).notNull(),
+    createdAt: timestamp("created_at"),
+  },
+  (t) => [
+    index("idx_gold_pollutants_created_at").on(t.createdAt),
+    index("idx_gold_pollutants_place_id").on(t.placeId),
+    index("idx_gold_pollutants_place_id").on(t.sourceId),
+  ]
+);
 
 export const pollutantsRelations = relations(pollutantsSchema, ({ one }) => ({
   source: one(dataSourceSchema, {
--- src/weather/domain/weather.schema.ts
+++ src/weather/domain/weather.schema.ts
@@ -2,6 +2,7 @@ import { dataSourceSchema } from "@/data-source/domain/data-source.schema";
 import { placesSchema } from "@/places/domain/places.schema";
 import { relations } from "drizzle-orm";
 import {
+  index,
   integer,
   numeric,
   pgTable,
@@ -9,24 +10,28 @@ import {
   timestamp,
 } from "drizzle-orm/pg-core";
 
-export const weatherSchema = pgTable("weather", {
-  id: serial("id").primaryKey().notNull(),
-  placeId: integer("place_id")
-    .references(() => placesSchema.id)
-    .notNull(),
-  sourceId: integer("source_id")
-    .references(() => dataSourceSchema.id)
-    .notNull(),
-  temperature: numeric("temperature", { precision: 5, scale: 2 }).notNull(),
-  humidity: integer("humidity").notNull(),
-  pressure: integer("pression").notNull(),
-  windSpeed: numeric("wind_speed", { precision: 4, scale: 2 }).notNull(),
-  windDirection: integer("wind_direction").notNull(),
-  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
-  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-    () => new Date()
-  ),
-});
+export const weatherSchema = pgTable(
+  "weather",
+  {
+    id: serial("id").primaryKey().notNull(),
+    placeId: integer("place_id")
+      .references(() => placesSchema.id)
+      .notNull(),
+    sourceId: integer("source_id")
+      .references(() => dataSourceSchema.id)
+      .notNull(),
+    temperature: numeric("temperature", { precision: 5, scale: 2 }).notNull(),
+    humidity: integer("humidity").notNull(),
+    pressure: integer("pression").notNull(),
+    windSpeed: numeric("wind_speed", { precision: 4, scale: 2 }).notNull(),
+    windDirection: integer("wind_direction").notNull(),
+    createdAt: timestamp("created_at"),
+  },
+  (t) => [
+    index("idx_gold_weather_created_at").on(t.createdAt),
+    index("idx_gold_weather_place_id").on(t.placeId),
+  ]
+);
 
 export const weatherRelations = relations(weatherSchema, ({ one }) => ({
   place: one(placesSchema, {
