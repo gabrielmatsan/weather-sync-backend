a14d29758c374b0a8294492ec562c9052025a8bd
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-05-16 17:07:37 -0300

    feat: adiciona controlador de lugares favoritos às rotas

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,11 +1,13 @@
 import Elysia from "elysia";
 import { AuthController } from "./auth/infra/auth.controller";
+import { FavoritePlaceController } from "./favorite-places/infra/favorite-place.controller";
 import { MessageController } from "./messages/infra/message.controller";
 import { PlaceController } from "./places/infra/place.controller";
 import { UserController } from "./users/infra/user.controller";
 
 export const routes = new Elysia({ prefix: "/v1" })
-    .use(AuthController)
-    .use(MessageController)
-    .use(PlaceController)
-    .use(UserController);
+  .use(AuthController)
+  .use(MessageController)
+  .use(PlaceController)
+  .use(UserController)
+  .use(FavoritePlaceController);
