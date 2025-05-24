97b4e0e44d2d3dd9047f5361bb677697d47ff6d6
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-05-24 13:19:01 -0300

    feat: adiciona campo opcional 'name' ao tipo favoritePlaceType

--- src/favorite-places/domain/favorite-place.type.ts
+++ src/favorite-places/domain/favorite-place.type.ts
@@ -4,6 +4,7 @@ export const favoritePlaceType = t.Object({
   userId: t.String(),
   placeId: t.Number(),
   createdAt: t.Date(),
+  name: t.Optional(t.String()),
 });
 
 export type FavoritePlaceType = Static<typeof favoritePlaceType>;
